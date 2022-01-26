a5f066401aa779e857d66c2aa4c02438c5b93d1d
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-01-26 08:52:05 +0000

    Add an input queue

--- src/repl.js
+++ src/repl.js
@@ -1,3 +1,4 @@
+// wasm_bindgen's JS code expects our imported functions to be global
 window.webrepl_execute = webrepl_execute;
 window.webrepl_read_result = webrepl_read_result;
 import * as mock_repl from "./mock_repl.js";
@@ -7,7 +8,9 @@ const repl = {
   elemHistory: document.getElementById("history-text"),
   elemSourceInput: document.getElementById("source-input"),
 
+  inputQueue: [],
   inputHistory: [],
+
   textDecoder: new TextDecoder(),
   textEncoder: new TextEncoder(),
 
@@ -18,7 +21,7 @@ const repl = {
 };
 
 // Initialise
-repl.elemSourceInput.addEventListener("change", onPressEnter);
+repl.elemSourceInput.addEventListener("change", onInputChange);
 mock_repl.default().then((instance) => {
   repl.compiler = instance;
 });
@@ -30,9 +33,10 @@ async function webrepl_execute(app_bytes, app_memory_size_ptr) {
   const { buffer } = app.exports.memory;
   repl.result = { addr, buffer };
 
-  // Tell the compiler how large the app's memory is
-  // The app can grow its heap arbitrarily while running
-  // Write directly to memory to avoid having to convert from JsValue
+  // Tell Rust how much space to reserve for its copy of the app's memory buffer.
+  // (The app might have grown its heap while running, so the size is only known now.)
+  // Write the result to memory instead of returning, since wasm_bindgen only allows
+  // imported async functions to return JsValue, which has some conversion overhead.
   const compilerMemory32 = new Uint32Array(repl.compiler.memory.buffer);
   compilerMemory32[app_memory_size_ptr >> 2] = buffer.byteLength;
 }
@@ -45,26 +49,36 @@ function webrepl_read_result(buffer_alloc_addr) {
   return addr;
 }
 
-let locked = false;
-async function onPressEnter(event) {
-  if (locked) return;
-  locked = true;
+function onInputChange(event) {
   const inputText = event.target.value;
   event.target.value = "";
 
-  const historyIndex = createHistoryEntry(inputText);
-
-  let outputText;
-  let ok = true;
-  try {
-    outputText = await mock_repl.webrepl_run(inputText);
-  } catch (e) {
-    outputText = `${e}`;
-    ok = false;
+  repl.inputQueue.push(inputText);
+  if (repl.inputQueue.length === 1) {
+    processInputQueue();
   }
+}
 
-  updateHistoryEntry(historyIndex, ok, outputText);
-  locked = false;
+// Use a queue just in case we somehow get inputs very fast
+// This is definitely an edge case, but maybe could happen from copy/paste?
+// It allows us to rely on having only one input at a time
+async function processInputQueue() {
+  while (repl.inputQueue.length) {
+    const inputText = repl.inputQueue[0];
+    const historyIndex = createHistoryEntry(inputText);
+
+    let outputText;
+    let ok = true;
+    try {
+      outputText = await mock_repl.webrepl_run(inputText);
+    } catch (e) {
+      outputText = `${e}`;
+      ok = false;
+    }
+
+    updateHistoryEntry(historyIndex, ok, outputText);
+    repl.inputQueue.shift();
+  }
 }
 
 function createHistoryEntry(inputText) {
